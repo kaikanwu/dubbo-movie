cb6d08989208ea42ba4f2d28563f7b4ccf142176
kaikanwu <wukaikan@gmail.com>
2019-02-17 21:47:26 +0800

    update

--- guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -3,5 +3,22 @@ package com.stylefeng.guns.api.user;
 public interface UserAPI {
 
 
+    // 登陆接口
     boolean login(String username, String password);
+
+    // 注册接口
+    boolean register(UserModel userModel);
+
+    // 判断用户名是否存在
+    boolean checkUserName(String username);
+
+    // 获取用户信息 uuid: unique user id
+    UserInfoModel getUserInfo(int uuid);
+
+    // 更新用户信息
+    UserInfoModel updateUserInfo(UserInfoModel userInfoModel);
+
+
+
 }
+
