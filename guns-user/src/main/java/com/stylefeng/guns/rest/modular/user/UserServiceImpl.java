f971cdeb6862a53ee546a844fabe2580628e3017
kaikanwu <wukaikan@gmail.com>
2019-02-24 11:41:46 +0800

    add impl of user's login and register

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -1,28 +1,105 @@
 package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.api.user.UserAPI;
 import com.stylefeng.guns.api.user.UserInfoModel;
 import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Service(interfaceClass = UserAPI.class)
 public class UserServiceImpl implements UserAPI {
 
+
+    /**
+     * 忽略这里的错误
+     */
+    @Autowired
+    private MoocUserTMapper moocUserTMapper;
+
+
+    /**
+     *  注册功能的实现
+     * @param userModel 保存部分用户信息的类
+     * @return true or false
+     */
+    @Override
+    public boolean register(UserModel userModel) {
+
+        // 1. 获取注册信息：已经获得，就是传入的参数
+
+        // 2. 将注册信息实体转换为数据实体
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(userModel.getUsername());
+
+        // 2.1 特别注意密码的转换，明文密码必须转换为加密后的密码
+        // 实际开发中：MD5 混淆加密 + 盐值（或者使用 shiro 框架）， 我们在此次项目中直接使用 MD5Util 这个工具类
+        String md5PWD = MD5Util.encrypt(userModel.getPassword());
+        moocUserT.setUserPwd(md5PWD);
+
+        moocUserT.setAddress(userModel.getAddress());
+        moocUserT.setEmail(userModel.getEmail());
+        moocUserT.setUserPhone(userModel.getPhone());
+        // 3. 将数据实体存入数据库
+        Integer insert = moocUserTMapper.insert(moocUserT);
+        if (insert > 0) {
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+
+    /**
+     * 登陆功能的实现
+     * @param username 前端传入的账号
+     * @param password 前端传入的密码
+     * @return true:用户id, false: 0
+     */
     @Override
     public int login(String username, String password) {
+
+        // 根据登陆账号获取数据库信息
+
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(username);
+
+        MoocUserT result = moocUserTMapper.selectOne(moocUserT);
+        // 将获取到的结果，和加密后的密码做比对
+
+        if (result != null && result.getUuid() > 0) {
+            String dbPwd = result.getUserPwd();
+            String userPwd = MD5Util.encrypt(password);
+            if (userPwd.equals(dbPwd)) {
+
+                // 返回用户 id
+                return result.getUuid();
+            }
+        }
         return 0;
     }
 
-    @Override
-    public boolean register(UserModel userModel) {
-        return false;
-    }
+
 
     @Override
     public boolean checkUserName(String username) {
-        return false;
+
+        EntityWrapper<MoocUserT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("user_name", username);
+        Integer result = moocUserTMapper.selectCount(entityWrapper);
+        if (result != null && result > 0) {
+            // 当存在该 username 时，返回 flase
+            return false;
+        }else {
+            // 不存在时，返回 true
+            return true;
+        }
     }
 
     @Override
